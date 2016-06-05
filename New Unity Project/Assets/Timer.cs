410f6a2e65327fcc8952807f7dcc1666d2ed198c
ivypwu <1989ivy66@gmail.com>
2016-06-04 21:54:34 -0700

    @@

---
+++ New Unity Project/Assets/Timer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    public float Starthour = 0;
+
+    public float Endhour = 0;
+
+
+    private float time;
+    private float Endtime;
+    public Text CountText;
+
+    // Use this for initialization
+    void Start () {
+        /*game 1 min = real world 1 second*/
+        float hour = Endhour - Starthour;
+        Endtime = hour * 60;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*update time*/
+        time += Time.deltaTime;
+
+        float min = Mathf.Floor(time) % 60;
+        float hour = (Mathf.Floor(time) - min) / 60;
+
+        if(time > Endtime * 0.8)
+        {
+            /*this is bad but whatever*/
+            CountText.color = Color.red;
+        }
+
+        /*show the value on the screen*/
+        string buffer = "";
+        if(min < 10)
+        {
+            buffer = "0";
+        }
+
+        CountText.text = "Time : \n" + hour.ToString() + ":" + buffer + min.ToString();
+    }
+}
